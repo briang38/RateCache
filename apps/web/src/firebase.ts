45db470626a6c5b7db584bf56ce75811a73721c2
briang38 <brianmgutt@lewisu.edu>
2026-03-23 21:32:59 -0500

    update firebase file

--- apps/web/src/firebase.ts
+++ apps/web/src/firebase.ts
@@ -3,19 +3,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "REMOVED",
-  authDomain: "ratecache-34a84.firebaseapp.com",
-  projectId: "ratecache-34a84",
-  storageBucket: "ratecache-34a84.firebasestorage.app",
-  messagingSenderId: "56464029138",
-  appId: "1:56464029138:web:f02f7b117a30dc2536840b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
-
-
-
